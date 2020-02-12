f0d0522c89761e460bad3e4372f8b51a7215105c
alumno <alumno@utnfra.com.ar>
2020-02-12 20:32:51 -0300

    antes del receso

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,5 +1,15 @@
 function mostrar()
 {
+var edad;
+
+edad = document.getElementById("edad").value;
+
+if (edad>=18) {
+alert("Usted es un mayor de Edad")
+} else {
+
+alert("Usted es menor de Edad")   
+}
 //tomo la edad  
 
 
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+
+if (edad >17) {
+alert("Es mayor")
+} else {
+if (edad >13){
+alert("es adolescente")
+} else {
+alert("es menor")
+}
+}
 
 
 
